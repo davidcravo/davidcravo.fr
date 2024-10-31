e824ceaf23a9f4c23dd84e922cb7da9abf19591d
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2024-10-31 13:50:43 +0100

    Head : ajout de la meta description

--- components/head.php
+++ components/head.php
@@ -10,7 +10,8 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?= $title ?></title>
+        <title><?= htmlentities($title) ?></title>
+        <meta name="description" content="<?= htmlentities($description) ?>">
         <link rel="stylesheet" href="/assets/css/style_home.css">
         <link rel="stylesheet" href="/assets/css/style_body.css">
         <link rel="stylesheet" href="/assets/css/style_achievements.css">
